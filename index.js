ed5e00a3ff5c761c9a4fb4a551ece581b8382d93
Öznur <oznur@tuta.io>
2022-04-19 14:21:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+let drivers = [];
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (num) {
+  return function (fare) {
+    return num * fare;
+  };
+};
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, func) {
+  if (func == returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+  } else if (func == returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers);
+  }
+}
